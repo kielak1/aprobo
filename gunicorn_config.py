f51ad073d4d6c2b2ebb7e6157eac7a238637f23d
Tadeusz Kielak <tadeusz@kielak.com>
2024-12-08 17:27:30 +0000

    Zastosowano .gitignore i oczyszczono cache

--- gunicorn_config.py
+++
@@ -1,7 +0,0 @@
-import multiprocessing
-
-#bind = "127.0.0.1:8000"  # IP i port
-bind = "unix:/opt/aprobo/gunicorn.sock"
-workers = multiprocessing.cpu_count() * 2 + 1  # Ilość workerów
-accesslog = '/var/log/gunicorn/access-log'  # Logowanie dostępu, '-' oznacza stdout
-errorlog = '/var/log/gunicorn/error-log'  # Logowanie błędów, '-' oznacza stdout
